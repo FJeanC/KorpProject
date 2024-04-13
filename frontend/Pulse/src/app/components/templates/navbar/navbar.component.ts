63f0b36e67ee0b61a0ab69d26b51e8ee592ebc56
FJeanC <jeanfagiani1@gmail.com>
2024-04-13 20:32:43 -0300

    Melhora os ícones da págian de feed e profile

--- frontend/Pulse/src/app/components/templates/navbar/navbar.component.ts
+++ frontend/Pulse/src/app/components/templates/navbar/navbar.component.ts
@@ -3,12 +3,13 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
